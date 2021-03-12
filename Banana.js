f74b7be05c6bce979092e9a33b397cc34ecfb680
Achiever-12 <77918616+Achiever-12@users.noreply.github.com>
2021-03-12 15:04:31 +0530

    Update Banana.js

--- Banana.js
+++ Banana.js
@@ -1,16 +1,4 @@
-//© Achiever
-
-function _(selector){ 
-let self=document.querySelector(selector);
-
- self.text=function(text){
- if(text){
-     self.innerTEXT=text;
- }
- return self.innerTEXT;
- } 
- self.html=function(){
-     return self.innerHTML;
- }
- return self
+//© Achieve
+function rand(){
+Math.random()
 }
